2070ad9a7dcd6f3ce757ce68c0f73b5c9ff6957d
zach <zhughes35@yahoo.com>
2016-01-06 12:16:45 -0500

    Yeahhhhhhhhhhh

---
+++ homebackup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#This script is used to backup the  user's home directory
+cd /home
+tar -cf /var/tmp/homebackup.tar $USER
+
+rm /var/tmp/homebackup.tar.gz > /dev/null
+
+gzip /var/tmp/homebackup.tar
+
+scp /var/tmp/homebackup.tar.gz administrator@10.1.10.70:~
+
+echo "Job completed"
+echo
---
+++ users.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+clear
+name=$USER
+echo "The script is starting now"
+echo "Hello "$name"!"
+echo
+echo "i will fetch for you all the logged in users now"
+w
+echo
+echo "I am done now...thanks"
+COLOR="blue"
+echo
+echo "FYI my favorite color is $COLOR"
+echo
+
